e733f048fdaa1326dd26ad77ffc67247077eeb99
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-16 17:06:21 +0530

    mongoose integrated

--- index.js
+++ index.js
@@ -1,23 +1,34 @@
-const express = require('express');
-const cors = require('cors');
+let express = require("express"),
+  path = require("path"),
+  mongoose = require("mongoose"),
+  cors = require("cors"),
+  bodyParser = require("body-parser");
 
+  //connect to db
+
+
+  //import the routes that talk to the db 
+
+  const bookRoute = require("./routes/book.routes");
+
+    // create an express application
 const serverapp = express();
 
 var corsOptions = {
-    origin: "http://localhost:5000"
-}
+  origin: "http://localhost:5000",
+};
 
 serverapp.use(cors(corsOptions));
 
 serverapp.use(express.json());
 
 //route
-serverapp.get("/",(req,res) => {
-    res.json({message:'your server is active here'});
-})
+serverapp.get("/", (req, res) => {
+  res.json({ message: "your server is active here" });
+});
 
 const PORT = process.env.PORT || 5000;
 
 serverapp.listen(PORT, () => {
-    console.log('server is active here')
-})
+  console.log("server is active here");
+});
--- routes/book.routes.js
+++ routes/book.routes.js
@@ -1,1 +1,20 @@
-const express = 
+const express = require("express");
+const app = express();
+
+const bookRoute = express.Router();
+let Book = require("../model/Book");
+
+
+// Get All Book
+bookRoute.route('/').get((req, res) => {
+    Book.find((error, data) => {
+    if (error) {
+      return next(error)
+    } else {
+      res.json(data)
+    }
+  })
+})
+
+module.exports = bookRoute;
+
