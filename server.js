c631719aebe1c418dc62ebc32820cea34ea9f253
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-16 16:33:54 +0530

    Initial commit

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const cors = require('cors');
+
+const serverapp = express();
+
+var corsOptions = {
+    origin: "http://localhost:5000"
+}
+
+serverapp.use(cors(corsOptions));
+
+serverapp.use(express.json());
+
+//route
+server.get("/",(req,res) => {
+    res.json({message:'your server is active here'});
+})
+
+const PORT = process.env.PORT || 5000;
+
+serverapp.listen(PORT, () => {
+    console.log('server is active here')
+})
