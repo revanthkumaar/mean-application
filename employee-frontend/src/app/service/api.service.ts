ffb97bde2b59fdc7a2c9f500eab75b69d171c4f5
revanthkumaar <iamrevanthkumar@gmail.com>
2021-07-19 15:59:17 +0530

    checkin with service

--- employee-frontend/src/app/service/api.service.ts
+++ employee-frontend/src/app/service/api.service.ts
@@ -1,9 +1,49 @@
 import { Injectable } from '@angular/core';
+import { Observable, throwError } from 'rxjs';
+import { catchError, map } from 'rxjs/operators';
+import {
+  HttpClient,
+  HttpHeaders,
+  HttpErrorResponse,
+} from '@angular/common/http';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiService {
+  baseUri: string = 'http://localhost:4000/api';
+  headers = new HttpHeaders().set('Content-Type', 'application/json');
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
+
+  //create
+  createEmployee(data): Observable<any> {
+    let url = `${this.baseUri}/create`;
+    this.http.post(url, data).pipe(catchError(this.errorMgmt));
+  }
+
+  //get complete information
+
+  // Get all employees
+  getEmployees() {
+    return this.http.get(`${this.baseUri}`);
+  }
+
+  //get single employee info
+
+  //update
+
+  //delete
+
+  //error handling
+  errorMgmt(error: HttpErrorResponse) {
+    let errorMessage = '';
+    if (error.error instanceof Errorevent) {
+      errorMessage = error.error.message;
+    } else {
+      errorMessage = `Error code: ${error.status}\nMessage: ${error.message}`;
+    }
+    console.log(errorMessage);
+    return throwError(errorMessage);
+  }
 }
